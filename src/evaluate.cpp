fd4d0702722c698e5e763d7a2e014ba4997b523c
mojinumber1 <mojinumber1@gmail.com>
2017-01-09 04:18:26 +0330

    Small bonus if the opponent has backward pawn and it is under our attack

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -202,6 +202,7 @@ namespace {
   const Score Hanging             = S(48, 27);
   const Score ThreatByPawnPush    = S(38, 22);
   const Score HinderPassedPawn    = S( 7,  0);
+  const Score BackwardThreat      = S( 5,  3);
 
   // Penalty for a bishop on a1/h1 (a8/h8 for black) which is trapped by
   // a friendly pawn on b2/g2 (b7/g7 for black). This can obviously only
@@ -517,8 +518,30 @@ namespace {
     const Bitboard TRank2BB = (Us == WHITE ? Rank2BB    : Rank7BB);
     const Bitboard TRank7BB = (Us == WHITE ? Rank7BB    : Rank2BB);
 
-    Bitboard b, weak, defended, safeThreats;
+    Bitboard b, weak, defended, safeThreats, backwardpawns;
     Score score = SCORE_ZERO;
+
+    // Small bonus if the opponent has backward pawn and it is under our attack
+    backwardpawns = ei.pi->backward_pawns(Them);
+    if (backwardpawns)
+    {
+    	Bitboard aa = ei.attackedBy[Us][QUEEN]
+                    | ei.attackedBy[Us][ROOK]
+                    | ei.attackedBy[Us][BISHOP]
+                    | ei.attackedBy[Us][KNIGHT];
+
+    	b = backwardpawns & aa;
+    	while (b)
+    	{
+    	Square s = 	pop_lsb(&b);
+    	for (PieceType i = KNIGHT; i < KING; ++i)
+    	{
+    		if (ei.attackedBy[Us][i] & s)
+    	    score += BackwardThreat;
+    	}
+    	}
+    }
+
 
     // Small bonus if the opponent has loose pawns or pieces
     if (   (pos.pieces(Them) ^ pos.pieces(Them, QUEEN, KING))
